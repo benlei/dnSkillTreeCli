4e7cec004aaf75b19e7667886fcbacc2d59a0e3e
Benjamin Lei <benlei.cse@gmail.com>
2015-11-03 23:42:33 -0800

    Fixed up script; it displays something that looks right!

--- heroku.js
+++ heroku.js
@@ -56,9 +56,6 @@ var compile = function() { // DuplicatedSkillType = for checking if in same 'gro
         uistring[parseInt(e.getAttribute("mid"))] = e.getFirstChild().getData()
     }
 
-    print(JSON.stringify(skills[0], null, 2))
-    exit()
-
     jsons = []
     jobs.filter(function(job) job.Service).forEach(function(job) {
         // fix a few things
@@ -95,47 +92,55 @@ var compile = function() { // DuplicatedSkillType = for checking if in same 'gro
         jobSkills = skills.filter(function(s) s.NeedJob == job.ID)
         jobSkillsID = jobSkills.map(function(s) s.ID)
         skillTree.filter(function(t) jobSkillsID.indexOf(t.SkillTableID) > -1).forEach(function(t) {
-            job.SkillTree[t.TreeSlotIndex] = t.SkillTableID
+            json.SkillTree[t.TreeSlotIndex] = t.SkillTableID
 
             // setup initial Skills with job sp req
-            job.Skills[t.SkillTableID] = {
-                NeedBasicSP: t.NeedBasicSP1,
-                NeedFirstSP: t.NeedFirstSP1,
-                NeedSecondSP: t.NeedSecondSP1,
+            json.Skills[t.SkillTableID] = {
+                NeedSP: [t.NeedBasicSP1, t.NeedFirstSP1, t.NeedSecondSP1]
             }
+
+            var skill = json.Skills[t.SkillTableID]
 
             // setup the parent job hash
             if (t.ParentSkillID1 > 0) {
-                job.Skills[t.SkillTableID].ParentSkills = {}
-                job.Skills[t.SkillTableID].ParentSkills[t.ParentSkillID1] = t.NeedParentSkillLevel1
+                skill.ParentSkills = {}
+                skill.ParentSkills[t.ParentSkillID1] = t.NeedParentSkillLevel1
             }
 
             if (t.ParentSkillID2 > 0) {
-                job.Skills[t.SkillTableID].ParentSkills[t.ParentSkillID2] = t.NeedParentSkillLevel2
+                skill.ParentSkills[t.ParentSkillID2] = t.NeedParentSkillLevel2
             }
 
             if (t.ParentSkillID3 > 0) {
-                job.Skills[t.SkillTableID].ParentSkills[t.ParentSkillID3] = t.NeedParentSkillLevel3
+                skill.ParentSkills[t.ParentSkillID3] = t.NeedParentSkillLevel3
             }
         })
 
         // setup skill levels
         jobSkills.forEach(function(s) {
-            levels = skillLevels.filter(function(l) jobSkillsID.indexOf(l.SkillIndex))
-            job.Skills[s.ID].Name = uistring[s.NameID]
-            job.Skills[s.ID].MaxLevel = s.MaxLevel
-            job.Skills[s.ID].SPMaxLevel = s.SPMaxLevel
-            job.Skills[s.ID].IconImageIndex = s.IconImageIndex
-            job.Skills[s.ID].SkillType = s.SkillType
-            job.Skills[s.ID].SkillDuplicate = s.SkillDuplicate
+            var levels = skillLevels.filter(function(l) jobSkillsID.indexOf(l.SkillIndex))
+            var skill = json.Skills[s.ID]
+            skill.Name = uistring[s.NameID]
+            skill.MaxLevel = s.MaxLevel
+            skill.SPMaxLevel = s.SPMaxLevel
+            skill.IconImageIndex = s.IconImageIndex
+            skill.SkillType = s.SkillType
+            skill.Levels = []
+
+            // SkillDuplicate is uncommon
+            if (s.SkillDuplicate > 0) {
+                skill.SkillDuplicate = s.SkillDuplicate
+            }
+
+            // weapons can be uncommon + order doesn't matter
             if (s.NeedWeaponType1 > -1 || s.NeedWeaponType2 > -1) {
-                job.Skills[s.ID].NeedWeaponType = []
+                skill.NeedWeaponType = []
                 if (s.NeedWeaponType1 > -1) {
-                    job.Skills[s.ID].NeedWeaponType.push(s.NeedWeaponType1)
+                    skill.NeedWeaponType.push(s.NeedWeaponType1)
                 }
 
                 if (s.NeedWeaponType2 > -1) {
-                    job.Skills[s.ID].NeedWeaponType.push(s.NeedWeaponType2)
+                    skill.NeedWeaponType.push(s.NeedWeaponType2)
                 }
             }
 
@@ -151,12 +156,13 @@ var compile = function() { // DuplicatedSkillType = for checking if in same 'gro
         })
 
         // setup levels
+        print(JSON.stringify(json, null, 2))
+
         exit()
 
         jsons.push(json)
     })
 
-    print(JSON.stringify(skills, null, 2))
 
 
 
