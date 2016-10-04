e96fc26c92a21a80d73e10108f7ee02c50162a6a
Benjamin Lei <benlei.cse@gmail.com>
2016-10-03 22:51:41 -0700

    Making the dnt.js a very simple example, while the maze.js should be the complex thing.

--- example/dnt.js
+++ example/dnt.js
@@ -1,125 +1,55 @@
 // imports
 var System = java.lang.System;
 var DriverManager = java.sql.DriverManager;
-var FileOutputStream = java.io.FileOutputStream;
-var FileInputStream = java.io.FileInputStream;
-var File = java.io.File;
-var IOUtils = org.apache.commons.io.IOUtils;
-var StandardCharsets = java.nio.charset.StandardCharsets;
-
-// static final fields
-var QUERY_GET_ALL_JOBS = "SELECT j.*, m._Message as JobName FROM Job j JOIN Message m ON m.ID = j._JobName WHERE _Service IS TRUE";
 
 // fields
-var config = JSON.parse(
-    IOUtils.toString(
-        new FileInputStream(System.getProperty("dnt.config")),
-        StandardCharsets.UTF_8));
-
 var connection;
 
+/** Normalize file name to table name. */
 var normalizeName = function (name) {
     if (name == 'uistring') {
         return 'message';
     }
 
-    var tableIdx = name.indexOf('table');
-
-    /* Capitalize the words */
-    return name.substring(0, tableIdx)
-        .replace('skill', 'Skill')
-        .replace('tree', 'Tree')
-        .replace('level', 'Level')
-        .replace('glyph', 'Glyph')
-        .replace('weapon', 'Weapon')
-        .replace('hero', 'Hero')
-        .replace('potential', 'Potential')
-        .replace('battlefield', 'Battlefield')
-        .replace('rank', 'Rank')
-        .replace('reward', 'Reward');
+    return name.substring(0, name.indexOf('table'));
 };
 
+/** Gets a JDBC connection. */
 var getConnection = function () {
     if (connection) {
         return connection;
     }
 
-    connection = DriverManager.getConnection("jdbc:mysql://localhost/maze?"
-        + "user=root&"
-        + "password=root&"
-        + "useUnicode=true&"
-        + "characterEncoding=utf-8&"
-        + "useSSL=false");
+    // connection = DriverManager.getConnection("jdbc:mysql://localhost/maze?"
+    //     + "user=root&"
+    //     + "password=root&"
+    //     + "useUnicode=true&"
+    //     + "characterEncoding=utf-8&"
+    //     + "useSSL=false");
 
-    // connection = DriverManager.getConnection("jdbc:h2:mem:test;MODE=MYSQL;IGNORECASE=TRUE",
-    //     "sa", "sa");
+    connection = DriverManager.getConnection("jdbc:h2:mem:test;MODE=MYSQL;IGNORECASE=TRUE");
 
     return connection;
 };
 
+/** Process stuff, if desired. Otherwise you can give it an empty body. */
 var process = function () {
     var connection = getConnection();
     var stmt = connection.createStatement();
-    var maxSP;
 
-    if (config.levelCap < 51) {
-        maxSP = (config.levelCap - 1) * 3;
-    } else {
-        maxSP = 147 + (config.levelCap - 50) * 2;
-    }
+    // Get all existing jobs
+    var rs = stmt.executeQuery("SELECT * FROM Job WHERE _Service IS TRUE");
 
-    // 1. Get all existing jobs
-    var rs = stmt.executeQuery(QUERY_GET_ALL_JOBS);
-
-    var jobs = {"jobs": {}, "classes": []};
+    // List
     while (rs.next()) {
-        var job = {
-            "id": rs.getInt('ID'),
-            "jobName": rs.getString('JobName'),
-            "jobNumber": rs.getInt('_JobNumber'),
-            "baseClass": rs.getInt('_BaseClass'),
-            "parentJob": rs.getInt('_ParentJob'),
-            "englishName": rs.getString('_EnglishName').toLowerCase(),
-            "jobIcon": rs.getInt('_JobIcon'),
-            "awakened": rs.getInt('_AwakeningItem') > 0,
-
-            // these 3 should be stored elsewhere...
-            "maxSPJob0": Math.floor(maxSP * rs.getFloat('_MaxSPJob0')),
-            "maxSPJob1": Math.floor(maxSP * rs.getFloat('_MaxSPJob1')),
-            "maxSPJob2": Math.floor(maxSP * rs.getFloat('_MaxSPJob2'))
-        };
-
-        if (job.jobNumber == 2) {
-            jobs.classes.push(job.id.toString());
-        }
-
-        jobs.jobs[job.id] = job;
+        System.out.println(rs.getInt('ID') + ' = ' + rs.getString('_EnglishName'));
     }
 
+    // Don't forget to close a statement
     stmt.close();
-
-    write("job.json", jobs);
-
-
-
-
-    // 2. For every job in jobs, get all their skills, their skill levels, and their skill tree
-
-    // 3. Find out all names used for weapons
-
-    // 4. Determine max SP possible for this cap. Include the Hero Level stuff.
-
-    // 5.
 };
 
+/** Close the JDBC connection (automatically run at end of program). */
 var close = function () {
-    var connection = getConnection();
     connection.close();
-};
-
-
-var write = function (path, json) {
-    var out = new FileOutputStream(new File(config.output, path));
-    out.write(JSON.stringify(json).getBytes(StandardCharsets.UTF_8));
-    out.close();
 };
---
+++ example/maze.js
@@ -0,0 +1,109 @@
+// imports
+var System = java.lang.System;
+var DriverManager = java.sql.DriverManager;
+var FileOutputStream = java.io.FileOutputStream;
+var FileInputStream = java.io.FileInputStream;
+var File = java.io.File;
+var IOUtils = org.apache.commons.io.IOUtils;
+var StandardCharsets = java.nio.charset.StandardCharsets;
+
+// static final fields
+var QUERY_GET_ALL_JOBS = "SELECT j.*, m._Message as JobName FROM Job j JOIN Message m ON m.ID = j._JobName WHERE _Service IS TRUE";
+
+// fields
+var config = JSON.parse(
+    IOUtils.toString(
+        new FileInputStream(System.getProperty("dnt.config")),
+        StandardCharsets.UTF_8));
+
+var connection;
+
+var normalizeName = function (name) {
+    if (name == 'uistring') {
+        return 'message';
+    }
+
+    return name.substring(0, name.indexOf('table'));
+};
+
+var getConnection = function () {
+    if (connection) {
+        return connection;
+    }
+
+    connection = DriverManager.getConnection("jdbc:mysql://localhost/maze?"
+        + "user=root&"
+        + "password=root&"
+        + "useUnicode=true&"
+        + "characterEncoding=utf-8&"
+        + "useSSL=false");
+
+    // connection = DriverManager.getConnection("jdbc:h2:mem:test;MODE=MYSQL;IGNORECASE=TRUE");
+
+    return connection;
+};
+
+var process = function () {
+    var connection = getConnection();
+    var stmt = connection.createStatement();
+    var maxSP;
+
+    if (config.levelCap < 51) {
+        maxSP = (config.levelCap - 1) * 3;
+    } else {
+        maxSP = 147 + (config.levelCap - 50) * 2;
+    }
+
+    // 1. Get all existing jobs
+    var rs = stmt.executeQuery(QUERY_GET_ALL_JOBS);
+
+    var jobs = {"jobs": {}, "classes": []};
+    while (rs.next()) {
+        var job = {
+            "id": rs.getInt('ID'),
+            "jobName": rs.getString('JobName'),
+            "jobNumber": rs.getInt('_JobNumber'),
+            "baseClass": rs.getInt('_BaseClass'),
+            "parentJob": rs.getInt('_ParentJob'),
+            "englishName": rs.getString('_EnglishName').toLowerCase(),
+            "jobIcon": rs.getInt('_JobIcon'),
+            "awakened": rs.getInt('_AwakeningItem') > 0,
+
+            // these 3 should be stored elsewhere...
+            "maxSPJob0": Math.floor(maxSP * rs.getFloat('_MaxSPJob0')),
+            "maxSPJob1": Math.floor(maxSP * rs.getFloat('_MaxSPJob1')),
+            "maxSPJob2": Math.floor(maxSP * rs.getFloat('_MaxSPJob2'))
+        };
+
+        if (job.jobNumber == 2) {
+            jobs.classes.push(job.id.toString());
+        }
+
+        jobs.jobs[job.id] = job;
+    }
+
+    stmt.close();
+
+    write("job.json", jobs);
+
+
+    // 2. For every job in jobs, get all their skills, their skill levels, and their skill tree
+
+    // 3. Find out all names used for weapons
+
+    // 4. Determine max SP possible for this cap. Include the Hero Level stuff.
+
+    // 5.
+};
+
+var close = function () {
+    var connection = getConnection();
+    connection.close();
+};
+
+
+var write = function (path, json) {
+    var out = new FileOutputStream(new File(config.output, path));
+    out.write(JSON.stringify(json).getBytes(StandardCharsets.UTF_8));
+    out.close();
+};
